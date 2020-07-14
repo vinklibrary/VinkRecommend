0a39949cf52d6453c20037e435f0dee91ba24a96
沈旭东 <shenxudong3@corp.netease.com>
2020-07-14 19:39:36 +0800

    update

--- Embeddings/Utils/walker.py
+++ Embeddings/Utils/walker.py
@@ -6,3 +6,16 @@
 '''
 
 
+class RandomWalker:
+    def __init__(self, G, p=1, q=1):
+        """
+
+        :param G:
+        :param p: Return parameter, controls the likelihood of immediately revisiting a node in the walk.
+        :param q: In-out parameter,allows the search to differentiate between "inward" and "outward" needs.
+        """
+        self.G = G
+        self.p = p
+        self.q = q
+
+    def
---
+++ Recalls/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+'''
+@File : __init__.py.py
+@Date : 2020/7/13
+@Auther : shenxudong3
+'''
